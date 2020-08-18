020136ef0782437f2e24c5baeb4597ba008bbfa5
lauphern <lau.p.hern@hotmail.com>
2020-08-19 01:10:34 +0200

    finished portfolio route:

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Project = require("../models/Project");
 
 router.get("/portfolio", (req, res) => {
-  res.send("portfolio")
+  Project.find({language: req.preferredLanguage})
+  .then(result => {
+    res.json(result);
+  })
+  .catch(err => {
+    next(err);
+  })
 })
 
 module.exports = router;
