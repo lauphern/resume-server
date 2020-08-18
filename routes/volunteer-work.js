e0fb09e0144331f7b444dfda83bde29f4927efac
lauphern <lau.p.hern@hotmail.com>
2020-08-19 01:12:02 +0200

    finished volunteer work route

--- routes/volunteer-work.js
+++ routes/volunteer-work.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Job = require("../models/Job");
 
 router.get("/volunteer-work", (req, res) => {
-  res.send("volunteer work")
+  Job.find({language: req.preferredLanguage})
+  .then(result => {
+    res.json(result);
+  })
+  .catch(err => {
+    next(err);
+  })
 })
 
 module.exports = router;
