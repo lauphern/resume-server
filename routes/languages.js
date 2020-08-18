989eb6ad08a4da8d7b004062f4a21d2a881d30af
lauphern <lau.p.hern@hotmail.com>
2020-08-19 01:08:10 +0200

    finished languages route

--- routes/languages.js
+++ routes/languages.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const { json } = require("body-parser");
 const router = express.Router();
 
 router.get("/languages", (req, res) => {
-  res.send("languages")
+  if(req.preferredLanguage === "es") {
+    res.json({
+      "Español": "lengua maternal",
+      "Inglés": "C1 APTIS British Council"
+    })
+  } else {
+    res.json({
+      Spanish: "mother tongue",
+      English: "C1 APTIS British Council"
+    })
+  }
 })
 
 module.exports = router;
