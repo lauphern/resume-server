04d7ad80ce4a4f8d75b3401f933a976e527c6301
lauphern <lau.p.hern@hotmail.com>
2020-08-18 20:54:41 +0200

    added error and 404 handlers

--- server.js
+++ server.js
@@ -11,7 +11,29 @@ const app = express();
 mongoose.connect(process.env.DB, { useNewUrlParser: true , useUnifiedTopology: true}, (err, db) => {
   if(err) console.error(err)
   else {
-    console.log(db)
+    console.log("Database connected");
+    const User = require("./models/User");
+
+    // app.use("/", (req, res) => {
+    //   const username = "hola";
+    //   const password = "contra";
+    //   User.findOne({username: username})
+    //   .then(result => {
+    //     if(result) throw new Error();
+    //     else {
+    //       bcrypt.hash(password, 10, (err, hash) => {
+    //         if(err) throw new Error(err);
+    //         else {
+    //           return User.create({username, password: hash})
+    //         }
+    //       })
+    //     }
+    //   })
+    //   .then(result => {
+    //     res.json({newUser: result});
+    //   })
+    //   .catch(err => console.error(err))
+    // })
   }
 });
 
@@ -28,6 +50,15 @@ app.use("/api/v1", require("./routes/hard-skills"));
 app.use("/api/v1", require("./routes/soft-skills"));
 app.use("/api/v1", require("./routes/languages"));
 app.use("/api/v1", require("./routes/portfolio"));
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+});
 
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server listening on port ${process.env.PORT}`))
