4f691a635ebe89b0f1ea2f2d552233a9366c218b
lauphern <lau.p.hern@hotmail.com>
2020-08-19 01:00:55 +0200

    finished certifications route

--- routes/certifications.js
+++ routes/certifications.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const EducationItem = require('../models/EducationItem');
 
-router.get("/certifications", (req, res) => {
-  res.send("certifications")
+router.get("/certifications", (req, res, next) => {
+  EducationItem.findOne({level: "certification", language: req.preferredLanguage})
+  .then(result => {
+    res.json(result);
+  })
+  .catch(err => {
+    next(err);
+  })
 })
 
 module.exports = router;
