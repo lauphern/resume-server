405e1213d5053a638a579f323bddc863afec52d7
lauphern <lau.p.hern@hotmail.com>
2020-08-19 01:10:50 +0200

    finished professional experience route

--- routes/professional-experience.js
+++ routes/professional-experience.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Job = require("../models/Job");
 
 router.get("/professional-experience", (req, res) => {
-  res.send("professional experience")
+  Job.find({language: req.preferredLanguage})
+  .then(result => {
+    res.json(result);
+  })
+  .catch(err => {
+    next(err);
+  })
 })
 
 module.exports = router;
