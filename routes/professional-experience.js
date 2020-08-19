e14f221e216fdd1626b9d878f6744f4d250b615a
lauphern <lau.p.hern@hotmail.com>
2020-08-19 18:49:05 +0200

    implemented year query

--- routes/professional-experience.js
+++ routes/professional-experience.js
@@ -2,15 +2,34 @@ const express = require("express");
 const router = express.Router();
 const Job = require("../models/Job");
 
+router.get("/professional-experience", (req, res, next) => {
+  if (!req.query.year) next();
+  else {
+    const { year } = req.query
+    Job.find({ volunteer: false, language: req.app.locals.preferredLanguage })
+      .then(result => {
+        let filtered = result.filter(el => {
+          return ((el.start_date >= new Date(year,0,1) && el.start_date <= new Date(year,11,31)) ||
+          (el.end_date >= new Date(year,0,1) && el.end_date <= new Date(year,11,31)))
+        })
+        filtered.sort((a, b) => b.start_date - a.start_date);
+        res.json(filtered);
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
+});
+
 router.get("/professional-experience", (req, res) => {
-  Job.find({volunteer: false, language: req.app.locals.preferredLanguage})
-  .then(result => {
-    result.sort((a, b) => b.start_date - a.start_date);
-    res.json(result);
-  })
-  .catch(err => {
-    next(err);
-  })
-})
+  Job.find({ volunteer: false, language: req.app.locals.preferredLanguage })
+    .then(result => {
+      result.sort((a, b) => b.start_date - a.start_date);
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
