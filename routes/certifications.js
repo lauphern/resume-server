47d981a3ba6d98b9616f7c2fa4f338f9cef487a0
lauphern <lau.p.hern@hotmail.com>
2020-08-19 18:23:11 +0200

    implemented sort before sending the data to the client

--- routes/certifications.js
+++ routes/certifications.js
@@ -5,6 +5,7 @@ const EducationItem = require('../models/EducationItem');
 router.get("/certifications", (req, res, next) => {
   EducationItem.find({level: "certification", language: req.app.locals.preferredLanguage})
   .then(result => {
+    result.sort((a, b) => a.start_date - b.start_date);
     res.json(result);
   })
   .catch(err => {
