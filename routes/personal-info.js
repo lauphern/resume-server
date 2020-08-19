e20951eee6d764e9b8c293986845e741e1ac00aa
lauphern <lau.p.hern@hotmail.com>
2020-08-19 12:02:45 +0200

    finished personal info route

--- routes/personal-info.js
+++ routes/personal-info.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/personal-info", (req, res) => {
-  //TODO
-  res.send("personal info")
+  const data = req.app.locals.data[req.app.locals.preferredLanguage || "en"];
+  res.json(data.personalInfo);
 })
 
 module.exports = router;
