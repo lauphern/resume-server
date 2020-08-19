15c43cbb142e9482ca647ca9bbe12d17915110df
lauphern <lau.p.hern@hotmail.com>
2020-08-19 19:23:19 +0200

    implemented query by type

--- routes/hard-skills.js
+++ routes/hard-skills.js
@@ -2,15 +2,28 @@ const express = require("express");
 const router = express.Router();
 const HardSkill = require("../models/HardSkill");
 
-//TODO add type param
+router.get("/hard-skills", (req, res, next) => {
+  if (!req.query.type) next();
+  else {
+    const { type } = req.query;
+    HardSkill.find({ type: type, language: req.app.locals.preferredLanguage })
+      .then(result => {
+        res.json(result);
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
+});
+
 router.get("/hard-skills", (req, res) => {
-  HardSkill.find({language: req.app.locals.preferredLanguage})
-  .then(result => {
-    res.json(result);
-  })
-  .catch(err => {
-    next(err);
-  })
-})
+  HardSkill.find({ language: req.app.locals.preferredLanguage })
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
