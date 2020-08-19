e479657b26a5b6fbf1eb6c07bab3a99bc9dcf859
lauphern <lau.p.hern@hotmail.com>
2020-08-19 11:37:24 +0200

    added a file with the translations of the info that's not in the database

---
+++ data/basicInfo.js
@@ -0,0 +1,42 @@
+module.exports = {
+  data: {
+    es: {
+      personalInfo: {
+        name: "Laura Pascual Hernández",
+        job_title: "Desarrolladora full-stack | ReactJS",
+        location: "Madrid",
+        contact_info: {
+          email: "laura.pascual.h@hotmail.com",
+          linkedin: "https://www.linkedin.com/in/laura-pascual-hernandez/?locale=es_ES",
+          github: "https://github.com/lauphern",
+          personal_website: "http://laurapascual.com/",
+        },
+        bio: `Periodismo, comunicación audiovisual, diseño gráfico, desarrollo web... Todo esto me ha aportado algo valioso: entender la sociedad, dar rienda suelta a mi creatividad, saber realizar una comunicación efectiva, etc. Son pequeñas cosas que enriquecen mi experiencia como desarrolladora web: mi propósito es escribir código eficiente, siendo consciente del proceso global de la empresa.\nMe caracterizan mi pasión por aprender cosas nuevas y resolver problemas (debugging), junto con mi formación en ambientes internacionales y mi interés por conocer distintas formas de pensar. Además, durante mi experiencia como instructora en Ironhack Netherlands he aprendido a evaluar código ajeno, hacer valoraciones sobre la arquitectura de una aplicación y lidiar con la presión psicológica, tan importante para los principantes en programación.`,
+      },
+      introduction: "Esta es la Resume API",
+      languages: {
+        Español: "lengua maternal",
+        Inglés: "C1 APTIS British Council",
+      },
+    },
+    en: {
+      personalInfo: {
+        name: "Laura Pascual",
+        job_title: "Full-stack developer | ReactJS",
+        location: "Madrid",
+        contact_info: {
+          email: "laura.pascual.h@hotmail.com",
+          linkedin: "https://www.linkedin.com/in/laura-pascual-hernandez/?locale=en_US",
+          github: "https://github.com/lauphern",
+          personal_website: "http://laurapascual.com/",
+        },
+        bio: `Journalism, audiovisual communication, graphic design, web development... I have learned many valuable things from all these experiences: to understand society, to unleash my creativity, to communicate effectively, etc. These small things enrich me as a web developer: my goal is to write efficient code while being aware of the company’s overall process.\nMy passion for learning new things, solving problems and debugging, along with my background in international settings and my eagerness to know other ways of thinking define my work personality. In addition, I learned to do code reviews, to assess the architecture of an app and to deal with psychological pressure during my time as a coding instructor at Ironhack Netherlands.`,
+      },
+      introduction: "This is the Resume API",
+      languages: {
+        Spanish: "mother tongue",
+        English: "C1 APTIS British Council",
+      },
+    },
+  },
+};
