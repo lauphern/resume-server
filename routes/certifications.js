6f5cfee15af9b60b447729663a235ef28fd02b3d
lauphern <lau.p.hern@hotmail.com>
2020-08-19 18:25:07 +0200

    changed the sorting from asc to desc

--- routes/certifications.js
+++ routes/certifications.js
@@ -5,7 +5,7 @@ const EducationItem = require('../models/EducationItem');
 router.get("/certifications", (req, res, next) => {
   EducationItem.find({level: "certification", language: req.app.locals.preferredLanguage})
   .then(result => {
-    result.sort((a, b) => a.start_date - b.start_date);
+    result.sort((a, b) => b.start_date - a.start_date);
     res.json(result);
   })
   .catch(err => {
