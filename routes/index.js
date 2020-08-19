3721029ac78aefa1d00a501242ed2dbbcf4daa86
lauphern <lau.p.hern@hotmail.com>
2020-08-19 11:59:03 +0200

    updated index route to use the data saved in locals

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,21 @@ const e = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  if(req.app.locals.preferredLanguage === "es") {
-    res.json({
-      info: "Esta es la Resume API",
-      endpoints: [
-        `${process.env.DEV_URL}/personal-info`,
-        `${process.env.DEV_URL}/professional-experience`,
-        `${process.env.DEV_URL}/education`,
-        `${process.env.DEV_URL}/certifications`,
-        `${process.env.DEV_URL}/volunteer-work`,
-        `${process.env.DEV_URL}/hard-skills`,
-        `${process.env.DEV_URL}/soft-skills`,
-        `${process.env.DEV_URL}/languages`,
-        `${process.env.DEV_URL}/portfolio`,
-      ],
-    });
-  } else {
-    res.json({
-      info: "This is the Resume API",
-      endpoints: [
-        `${process.env.DEV_URL}/personal-info`,
-        `${process.env.DEV_URL}/professional-experience`,
-        `${process.env.DEV_URL}/education`,
-        `${process.env.DEV_URL}/certifications`,
-        `${process.env.DEV_URL}/volunteer-work`,
-        `${process.env.DEV_URL}/hard-skills`,
-        `${process.env.DEV_URL}/soft-skills`,
-        `${process.env.DEV_URL}/languages`,
-        `${process.env.DEV_URL}/portfolio`,
-      ],
-    });
-  }
+  const data = req.app.locals.data[req.app.locals.preferredLanguage || "en"];
+  res.json({
+    info: data.introduction,
+    endpoints: [
+      `${process.env.DEV_URL}/personal-info`,
+      `${process.env.DEV_URL}/professional-experience`,
+      `${process.env.DEV_URL}/education`,
+      `${process.env.DEV_URL}/certifications`,
+      `${process.env.DEV_URL}/volunteer-work`,
+      `${process.env.DEV_URL}/hard-skills`,
+      `${process.env.DEV_URL}/soft-skills`,
+      `${process.env.DEV_URL}/languages`,
+      `${process.env.DEV_URL}/portfolio`,
+    ],
+  });
 });
 
 module.exports = router;
