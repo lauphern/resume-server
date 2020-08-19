117a3b597fd46c55eaf9073845d68e744dfe6bae
lauphern <lau.p.hern@hotmail.com>
2020-08-19 18:26:34 +0200

    implemented sort before sending the data to the client

--- routes/professional-experience.js
+++ routes/professional-experience.js
@@ -5,6 +5,7 @@ const Job = require("../models/Job");
 router.get("/professional-experience", (req, res) => {
   Job.find({volunteer: false, language: req.app.locals.preferredLanguage})
   .then(result => {
+    result.sort((a, b) => b.start_date - a.start_date);
     res.json(result);
   })
   .catch(err => {
