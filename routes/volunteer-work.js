4a0d47e3123524820ba7b8d7e60df36b6c09086c
lauphern <lau.p.hern@hotmail.com>
2020-08-19 19:13:04 +0200

    querying by year

--- routes/volunteer-work.js
+++ routes/volunteer-work.js
@@ -4,14 +4,32 @@ const Job = require("../models/Job");
 
 //TODO add year param
 router.get("/volunteer-work", (req, res) => {
-  Job.find({volunteer: true, language: req.app.locals.preferredLanguage})
-  .then(result => {
-    result.sort((a, b) => b.start_date - a.start_date);
-    res.json(result);
-  })
-  .catch(err => {
-    next(err);
-  })
-})
+  if (!req.query.year) next();
+  else {
+    const { year } = req.query
+    Job.find({ volunteer: true, language: req.app.locals.preferredLanguage })
+      .then(result => {
+        let filtered = result.filter(el => {
+          return (el.start_date <= new Date(year,11,31) && el.end_date >= new Date(year,0,1))
+        })
+        filtered.sort((a, b) => b.start_date - a.start_date);
+        res.json(filtered);
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
+});
+
+router.get("/volunteer-work", (req, res) => {
+  Job.find({ volunteer: true, language: req.app.locals.preferredLanguage })
+    .then(result => {
+      result.sort((a, b) => b.start_date - a.start_date);
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
