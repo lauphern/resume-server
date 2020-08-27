83d064e7421abb6abbef2ac03b34c7cfcb3af37d
lauphern <lau.p.hern@hotmail.com>
2020-08-28 01:18:13 +0200

    replaced env variable URL_ENV for ENV

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ router.get("/", (req, res) => {
   res.json({
     info: data.introduction,
     endpoints: [
-      `${process.env.DEV_URL}/personal-info`,
-      `${process.env.DEV_URL}/download`,
-      `${process.env.DEV_URL}/professional-experience`,
-      `${process.env.DEV_URL}/education`,
-      `${process.env.DEV_URL}/certifications`,
-      `${process.env.DEV_URL}/volunteer-work`,
-      `${process.env.DEV_URL}/hard-skills`,
-      `${process.env.DEV_URL}/soft-skills`,
-      `${process.env.DEV_URL}/languages`,
-      `${process.env.DEV_URL}/portfolio`,
+      `${process.env.URL}/personal-info`,
+      `${process.env.URL}/download`,
+      `${process.env.URL}/professional-experience`,
+      `${process.env.URL}/education`,
+      `${process.env.URL}/certifications`,
+      `${process.env.URL}/volunteer-work`,
+      `${process.env.URL}/hard-skills`,
+      `${process.env.URL}/soft-skills`,
+      `${process.env.URL}/languages`,
+      `${process.env.URL}/portfolio`,
     ],
   });
 });
