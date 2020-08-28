7ca37752434e29e7f1a63c2d94f49e8ca08e9c4c
lauphern <lau.p.hern@hotmail.com>
2020-08-28 12:46:52 +0200

    modified education route to query with multiple parameters at the same time

--- routes/education.js
+++ routes/education.js
@@ -5,11 +5,10 @@ const EducationItem = require("../models/EducationItem");
 const { educationSearch } = require("../helpers/search");
 
 router.get("/education", (req, res, next) => {
-  if (!req.query.year) next();
-  else {
-    const { year } = req.query;
-    educationSearch({ model: EducationItem, language: req.app.locals.preferredLanguage })
-      .then(result => {
+  const { year, level } = req.query;
+  educationSearch({ model: EducationItem, language: req.app.locals.preferredLanguage, level: level})
+    .then(result => {
+      if(year) {
         let filtered = result.filter(el => {
           if (el.finished) {
             return el.start_date <= new Date(year, 11, 31) && el.end_date >= new Date(year, 0, 1);
@@ -19,33 +18,10 @@ router.get("/education", (req, res, next) => {
         });
         filtered.sort((a, b) => b.start_date - a.start_date);
         res.json(filtered);
-      })
-      .catch(err => {
-        next(err);
-      });
-  }
-});
-
-router.get("/education", (req, res, next) => {
-  if (!req.query.level) next();
-  else {
-    const { level } = req.query;
-    educationSearch({ model: EducationItem, language: req.app.locals.preferredLanguage, level })
-      .then(result => {
+      } else {
         result.sort((a, b) => b.start_date - a.start_date);
         res.json(result);
-      })
-      .catch(err => {
-        next(err);
-      });
-  }
-});
-
-router.get("/education", (req, res, next) => {
-  educationSearch({ model: EducationItem, language: req.app.locals.preferredLanguage })
-    .then(result => {
-      result.sort((a, b) => b.start_date - a.start_date);
-      res.json(result);
+      }
     })
     .catch(err => {
       next(err);
